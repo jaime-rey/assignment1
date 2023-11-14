43b1de49c0c9b67945586d92e1adb78b3e62dbec
jaime-rey <jaimereycasado@gmail.com>
2023-11-14 03:29:01 +0100

    first success

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 (function () {
-  ("use strict");
-  console.log("InitLunchCheckController");
+  "use strict";
   angular
     .module("LunchCheck", [])
-    .controller("LunchCheckController", LunchCheckController);
-
-  LunchCheckController.$inject = ["LunchCheckController"];
-  function LunchCheckController(LunchCheckController) {
-    let list = this;
-    list.listText = "";
-    list.checkText = function () {
+    .controller("LunchCheckController", function ($scope) {
       console.log("LunchCheckController");
-    };
-  }
+      $scope.listText = "";
+      $scope.listItemsNumber = 0;
+      $scope.listItems = [];
+      $scope.errorMessage = "test";
+
+      $scope.checkText = function () {
+        console.log("checkText");
+        $scope.listItems = $scope.listText.split(",");
+        $scope.listItemsNumber = $scope.listItems.length;
+        console.log("listItemsNumber: " + $scope.listItemsNumber);
+        if ($scope.listItemsNumber <= 3) {
+          console.log("cucucu " + $scope.listItemsNumber);
+          $scope.errorMessage = "ok with the list";
+        } else {
+          $scope.errorMessage = "not ok with the list";
+        }
+      };
+    });
 })();
